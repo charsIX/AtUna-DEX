e1271a236d7c1b36b06798fc12e98a0581086280
carloCecchetti <carlo.cecco0@gmail.com>
2022-02-17 18:54:52 +0100

    css

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ function showCoins($array){
 	for ($i=0; $i<count($array['data']['coins']); $i++)
 	{
 			?>
-					<option value="<?php print_r($array['data']['coins'][$i]['price'])."\n";?>"><?php print_r($array['data']['coins'][$i]['symbol'])."\n";;?></option>
+					<option value="<?php print_r($array['data']['coins'][$i]['price'])."\n";?>"><?php print_r($array['data']['coins'][$i]['symbol'])."\n";?></option>
 			<?php
 	}
 }
@@ -53,40 +53,50 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$newToken2Reserve = $k / $newToken1Reserve;
 	$token2Reserve = $k/$token1Reserve;
 
-
 	$out = $token2Reserve - $newToken2Reserve;
 
-	echo "$out";
+	// echo "$out";
 }
 ?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title><?php echo "AtUna @ ".date('H:i:s');?></title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <!-- navbar -->
-    <div class="main">
-      <div class="nav-bar">
-        <h4 class="title">AtUna DEX</h4>
-        <a href="#">click here</a>
-        <a href="#">click here</a>
-        <a href="#">click here</a>
-        <a href="#">click here</a>
-        <a href="#">click here</a>
-      </div>
+		<div class="main">
+			<!-- navbar -->
+			<div class="top">
+	      <div class="nav-bar">
+	        <a class="title" href="index.php">AtUna DEX</a>
+	        <a href="#">Click here</a>
+	        <a href="#">Click here</a>
+	        <a href="#">Click here</a>
+	        <a href="#">Click here</a>
+	        <a href="#">Click here</a>
+	      </div>
 
-      <div class="swap-zone">
-        <form class="" action="index.php" method="post">
-          <input type="number" name="in" value="" placeholder="Amount to swap" required oninvalid="this.setCustomValidity('Better if you provide an amount to swap')" onchange="this.setCustomValidity('')">
-          <select name="token1"><?php showCoins($array);?></select>
-					<output id"received" name="result"> <?php echo "$out"; ?> </output>
-					<select name="token2"><?php	showCoins($array);?></select>
-					<input type="number" name="tvl" value="" placeholder="TVL in $" required oninvalid="this.setCustomValidity('PI huge if no TVL, mate')" onchange="this.setCustomValidity('')">
-          <input id="swap-button" type="submit" name="" value="Swap">
-        </form>
-      </div>
-    </div>
+	      <div class="swap-zone">
+	        <form class="" action="index.php" method="post">
+	          <input class="fields" type="number" name="in" value=""
+						placeholder="Amount to swap" required
+						oninvalid="this.setCustomValidity('Better if you provide an amount to swap')"
+						onchange="this.setCustomValidity('')">
+	          <select class="fields" name="token1"><?php showCoins($array);?></select>
+						<br>
+						<input class="fields" type="number" name="" disabled value="<?php echo "$out";?>">
+						<select class="fields" name="token2"><?php	showCoins($array);?></select>
+						<br>
+						<input class="fields" type="number" name="tvl" value=""
+						placeholder="TVL in $" required
+						oninvalid="this.setCustomValidity('PI huge if no TVL, mate')"
+						onchange="this.setCustomValidity('')">
+	          <input class="fields" type="submit" name="" value="Swap">
+	        </form>
+	      </div>
+	    </div>
+		</div>
+
   </body>
 </html>
